08d433421515722f59100755b14e6f25d92118ad
Mario Alban <albanmario@gmail.com>
2016-11-15 23:16:55 -0500

    init

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'jade');
+app.get("/", function(req, res){
+	res.render("home")
+});
+
+app.listen(8080)
